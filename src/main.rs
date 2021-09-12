0c917e5458f6d806519b14d3bbcef93a585783e2
Jere Pehkonen <jeanpehk@gmail.com>
2021-09-12 17:04:48 +0300

    move constants

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,10 @@
 #![ allow( dead_code ) ]
 
 mod test;
+mod constants;
 
-const MEMSIZE: usize = 1024;
-const REG_NAMES: [&str; 33] = [
-    "zero",
-    "ra",
-    "sp",
-    "gp",
-    "tp",
-    "t0",
-    "t1",
-    "t2",
-    "s0",
-    "s1",
-    "a0",
-    "a1",
-    "a2",
-    "a3",
-    "a4",
-    "a5",
-    "a6",
-    "a7",
-    "s2",
-    "s3",
-    "s4",
-    "s5",
-    "s6",
-    "s7",
-    "s8",
-    "s9",
-    "s10",
-    "s11",
-    "t3",
-    "t4",
-    "t5",
-    "t6",
-    "pc"
-];
+use constants::MEMSIZE;
+use constants::REG_NAMES;
 
 /*
  * Main structure for core state
