0735709d1853438495d589343f51b9f6abbc637e
Jere Pehkonen <jeanpehk@gmail.com>
2021-09-24 22:45:42 +0300

    like to match

--- src/constants.rs
+++ src/constants.rs
@@ -34,3 +34,61 @@ pub const REG_NAMES: [&str; 33] = [
     "t6",
     "pc"
 ];
+
+pub mod opcodes {
+    pub const TEST: u32 = 0;
+    pub const OP: u32 = 0b0110011;
+    pub const OP_IMM: u32 = 0b0010011;
+    pub const LUI: u32 = 0b0110111;
+    pub const AUIPC: u32 = 0b0010111;
+    pub const JAL: u32 = 0b1101111;
+    pub const JALR: u32 = 0b1100111;
+    pub const BRANCH: u32 = 0b1100011;
+    pub const LOAD: u32 = 0b0000011;
+    pub const STORE: u32 = 0b0100011;
+    pub const MISCMEM: u32 = 0b0001111;
+}
+
+pub mod funct3 {
+    // Integer Register Immediate
+    pub const ADDI: u32 = 0b000;
+    pub const SLTI: u32 = 0b010;
+    pub const SLTIU: u32 = 0b011;
+    pub const ANDI: u32 = 0b111;
+    pub const ORI: u32 = 0b110;
+    pub const XORI: u32 = 0b100;
+    pub const SLLI: u32 = 0b001;
+    pub const SRXI: u32 = 0b101;
+
+    // Integer Register Register
+    pub const ADD: u32 = 0b000;
+    pub const SLT: u32 = 0b010;
+    pub const SLTU: u32 = 0b011;
+    pub const AND: u32 = 0b111;
+    pub const OR: u32 = 0b110;
+    pub const XOR: u32 = 0b100;
+    pub const SLL: u32 = 0b001;
+    // SRL or SRA
+    pub const SRX: u32 = 0b101;
+    pub const _SUB: u32 = 0b000;
+
+    // Branch
+    pub const BEQ: u32 = 0b000;
+    pub const BNE: u32 = 0b001;
+    pub const BLT: u32 = 0b100;
+    pub const BLTU: u32 = 0b110;
+    pub const BGE: u32 = 0b101;
+    pub const BGEU: u32 = 0b111;
+
+    // Load/Store
+    pub const LB: u32 = 0b000;
+    pub const LH: u32 = 0b001;
+    pub const LW: u32 = 0b010;
+    pub const LBU: u32 = 0b100;
+    pub const LHU: u32 = 0b101;
+    pub const SB: u32 = 0b000;
+    pub const SH: u32 = 0b001;
+    pub const SW: u32 = 0b010;
+
+    pub const FENCE: u32 = 0b000;
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod constants;
 
 use constants::MEMSIZE;
 use constants::REG_NAMES;
+use constants::funct3;
+use constants::opcodes;
 
 /*
  * Main structure for core state
@@ -12,74 +14,6 @@ use constants::REG_NAMES;
 struct Core {
     memory: [u8; MEMSIZE],
     regs: [i32;33]
-}
-
-enum Opcode {
-    Op = 0b0110011,
-    OpImm = 0b0010011,
-    Lui = 0b0110111,
-    Auipc = 0b0010111,
-    Jal = 0b1101111,
-    Jalr = 0b1100111,
-    Branch = 0b1100011,
-    Load = 0b0000011,
-    Store = 0b0100011,
-    MiscMem = 0b0001111
-}
-
-enum Funct3 {
-    Zero = 0b000,
-    One = 0b001,
-    Two = 0b010,
-    Three = 0b011,
-    Four = 0b100,
-    Five = 0b101,
-    Six = 0b110,
-    Seven = 0b111,
-}
-
-impl Funct3 {
-    // Integer Register Immediate
-    const ADDI: Funct3 = Funct3::Zero;
-    const SLTI: Funct3 = Funct3::Two;
-    const SLTIU: Funct3 = Funct3::Three;
-    const ANDI: Funct3 = Funct3::Seven;
-    const ORI: Funct3 = Funct3::Six;
-    const XORI: Funct3 = Funct3::Four;
-    const SLLI: Funct3 = Funct3::One;
-    const SRXI: Funct3 = Funct3::Five;
-
-    // Integer Register Register
-    const ADD: Funct3 = Funct3::Zero;
-    const SLT: Funct3 = Funct3::Two;
-    const SLTU: Funct3 = Funct3::Three;
-    const AND: Funct3 = Funct3::Seven;
-    const OR: Funct3 = Funct3::Six;
-    const XOR: Funct3 = Funct3::Four;
-    const SLL: Funct3 = Funct3::One;
-    // SRL or SRA
-    const SRX: Funct3 = Funct3::Five;
-    const _SUB: Funct3 = Funct3::Zero;
-
-    // Branch
-    const BEQ: Funct3 = Funct3::Zero;
-    const BNE: Funct3 = Funct3::One;
-    const BLT: Funct3 = Funct3::Four;
-    const BLTU: Funct3 = Funct3::Six;
-    const BGE: Funct3 = Funct3::Five;
-    const BGEU: Funct3 = Funct3::Seven;
-
-    // Load/Store
-    const LB: Funct3 = Funct3::Zero;
-    const LH: Funct3 = Funct3::One;
-    const LW: Funct3 = Funct3::Two;
-    const LBU: Funct3 = Funct3::Four;
-    const LHU: Funct3 = Funct3::Five;
-    const SB: Funct3 = Funct3::Zero;
-    const SH: Funct3 = Funct3::One;
-    const SW: Funct3 = Funct3::Two;
-
-    const FENCE: Funct3 = Funct3::Zero;
 }
 
 struct IType {
@@ -218,227 +152,229 @@ fn sign_extend(ins: u32, bits: u32) -> i32 {
 fn eval(ins: u32, mut core: Core) -> Core {
     let opcode = take_range(6, 0, ins);
 
-    if opcode == Opcode::OpImm as u32 {
-        let IType { imm, rs1, rd, funct3 } = get_i_type(ins);
-        let signed_imm = sign_extend(imm, 12);
+    match opcode {
+        opcodes::OP_IMM => {
+            let IType { imm, rs1, rd, funct3 } = get_i_type(ins);
+            let signed_imm = sign_extend(imm, 12);
 
-        if funct3 == Funct3::ADDI as u32 {
-            core.regs[rd] = core.regs[rs1] + signed_imm;
-        }
-        else if funct3 == Funct3::SLTI as u32 {
-            core.regs[rd] = if core.regs[rs1] < signed_imm {1} else {0};
-        }
-        else if funct3 == Funct3::SLTIU as u32 {
-            core.regs[rd] =
-                if (core.regs[rs1] as u32) < signed_imm as u32 {1} else {0};
-        }
-        else if funct3 == Funct3::ANDI as u32 {
-            core.regs[rd] = core.regs[rs1] & signed_imm;
-        }
-        else if funct3 == Funct3::ORI as u32 {
-            core.regs[rd] = core.regs[rs1] | signed_imm;
-        }
-        else if funct3 == Funct3::XORI as u32 {
-            core.regs[rd] = core.regs[rs1] ^ signed_imm;
-        }
-        else if funct3 == Funct3::SLLI as u32 {
-            let shamt = imm & 0b11111;
-            core.regs[rd] = core.regs[rs1] << shamt;
-        }
-        else if funct3 == Funct3::SRXI as u32 {
-            let arithmetic = take_range(30,30, ins);
-            let shamt = imm & 0b11111;
-            // SRAI
-            if arithmetic == 1 {
-                core.regs[rd] = core.regs[rs1] >> shamt;
+            match funct3 {
+                funct3::ADDI => {
+                    core.regs[rd] = core.regs[rs1] + signed_imm
+                },
+                funct3::SLTI => {
+                    core.regs[rd] = if core.regs[rs1] < signed_imm {1} else {0};
+                },
+                funct3::SLTIU => {
+                    core.regs[rd] =
+                        if (core.regs[rs1] as u32) < signed_imm as u32 {1} else {0};
+                },
+                funct3::ANDI => {
+                    core.regs[rd] = core.regs[rs1] & signed_imm;
+                },
+                funct3::ORI => {
+                    core.regs[rd] = core.regs[rs1] | signed_imm;
+                },
+                funct3::XORI => {
+                    core.regs[rd] = core.regs[rs1] ^ signed_imm;
+                },
+                funct3::SLLI => {
+                    let shamt = imm & 0b11111;
+                    core.regs[rd] = core.regs[rs1] << shamt;
+                },
+                funct3::SRXI => {
+                    let arithmetic = take_range(30,30, ins);
+                    let shamt = imm & 0b11111;
+                    if arithmetic == 1 { // SRAI
+                        core.regs[rd] = core.regs[rs1] >> shamt;
+                    }
+                    else { // SRLI
+                        let ans = core.regs[rs1] as u32 >> shamt;
+                        core.regs[rd] = ans as i32;
+                    }
+                },
+                _ => {
+                    println!("Unknown funct3 in op_imm: {}", funct3);
+                }
             }
-            // SRLI
-            else {
-                let ans = core.regs[rs1] as u32 >> shamt;
-                core.regs[rd] = ans as i32;
+        },
+        opcodes::OP => {
+            let RType { funct7, rs2, rs1, funct3, rd } = get_r_type(ins);
+            match funct3 {
+                funct3::ADD => {
+                    if funct7 == 0 { // add
+                        core.regs[rd] = core.regs[rs1] + core.regs[rs2];
+                    }
+                    else { // sub
+                        core.regs[rd] = core.regs[rs1] - core.regs[rs2];
+                    }
+                },
+                funct3::SLT => {
+                    core.regs[rd] = if core.regs[rs1] < core.regs[rs2] {1} else {0};
+                },
+                funct3::SLTU => {
+                    let (lhs, rhs) = (core.regs[rs1] as u32, core.regs[rs2] as u32);
+                    core.regs[rd] = if lhs < rhs {1} else {0};
+                },
+                funct3::XOR => {
+                    core.regs[rd] = core.regs[rs1] ^ core.regs[rs2];
+                },
+                funct3::SLL => {
+                    let shamt = core.regs[rs2] & 0b11111;
+                    core.regs[rd] = core.regs[rs1] << shamt;
+                },
+                funct3::SRX => {
+                    let shamt = core.regs[rs2] & 0b11111;
+                    core.regs[rd] = if funct7 == 0 { // SRL
+                        (core.regs[rs1] as u32 >> shamt) as i32
+                    }
+                    else { // SRA
+                        core.regs[rs1] >> shamt
+                    };
+                },
+                funct3::OR => {
+                    core.regs[rd] = core.regs[rs1] | core.regs[rs2];
+                },
+                funct3::AND => {
+                    core.regs[rd] = core.regs[rs1] & core.regs[rs2];
+                },
+                _ => {
+                    println!("Unknown Funct3 in Opcode Op: {}", funct3);
+                }
             }
-        }
-        else {
-            println!("Unknown funct3 in op_imm: {}", funct3);
-        }
-    }
-    else if opcode == Opcode::Op as u32 {
-        let RType { funct7, rs2, rs1, funct3, rd } = get_r_type(ins);
+        },
+        opcodes::LUI => {
+            let UType { rd, imm } = get_u_type(ins);
+            core.regs[rd] = (imm << 12) as i32;
+        },
+        opcodes::AUIPC => {
+            let UType { rd, imm } = get_u_type(ins);
+            core.regs[rd] = core.regs[32] + ((imm<<12) as i32);
+        },
+        opcodes::JAL => {
+            let JType { imm, rd } = get_j_type(ins);
+            let signed = sign_extend(imm, 21);
+            core.regs[rd] = core.regs[32]+4;
+            core.regs[32] = core.regs[32]+signed;
+        },
+        opcodes::JALR => {
+            let IType { imm, rs1, rd, funct3: _ } = get_i_type(ins);
 
-        // add or sub
-        if funct3 == Funct3::ADD as u32 {
-            // add
-            if funct7 == 0 {
-                core.regs[rd] = core.regs[rs1] + core.regs[rs2];
-            }
-            // sub
-            else {
-                core.regs[rd] = core.regs[rs1] - core.regs[rs2];
-            }
-        }
-        else if funct3 == Funct3::SLT as u32 {
-            core.regs[rd] = if core.regs[rs1] < core.regs[rs2] {1} else {0};
-        }
-        else if funct3 == Funct3::SLTU as u32 {
-            let (lhs, rhs) = (core.regs[rs1] as u32, core.regs[rs2] as u32);
-            core.regs[rd] = if lhs < rhs {1} else {0};
-        }
-        else if funct3 == Funct3::XOR as u32 {
-            core.regs[rd] = core.regs[rs1] ^ core.regs[rs2];
-        }
-        else if funct3 == Funct3::SLL as u32 {
-            let shamt = core.regs[rs2] & 0b11111;
-            core.regs[rd] = core.regs[rs1] << shamt;
-        }
-        else if funct3 == Funct3::SRX as u32 {
-            let shamt = core.regs[rs2] & 0b11111;
-            // SRL
-            core.regs[rd] = if funct7 == 0 {
-                (core.regs[rs1] as u32 >> shamt) as i32
-            }
-            // SRA
-            else {
-                core.regs[rs1] >> shamt
-            };
-        }
-        else if funct3 == Funct3::OR as u32 {
-            core.regs[rd] = core.regs[rs1] | core.regs[rs2];
-        }
-        else if funct3 == Funct3::AND as u32 {
-            core.regs[rd] = core.regs[rs1] & core.regs[rs2];
-        }
-        else {
-            println!("Unknown Funct3 in Opcode Op: {}", funct3);
-        }
-    }
-    else if opcode == Opcode::Lui as u32 {
-        let UType { rd, imm } = get_u_type(ins);
-        core.regs[rd] = (imm << 12) as i32;
-    }
-    else if opcode == Opcode::Auipc as u32 {
-        let UType { rd, imm } = get_u_type(ins);
-        core.regs[rd] = core.regs[32] + ((imm<<12) as i32);
-    }
-    else if opcode == Opcode::Jal as u32 {
-        let JType { imm, rd } = get_j_type(ins);
-        let signed = sign_extend(imm, 21);
-        core.regs[rd] = core.regs[32]+4;
-        core.regs[32] = core.regs[32]+signed;
-    }
-    else if opcode == Opcode::Jalr as u32 {
-        let IType { imm, rs1, rd, funct3: _ } = get_i_type(ins);
+            let imm = sign_extend(imm, 12);
+            let val = imm+core.regs[rs1];
 
-        let imm = sign_extend(imm, 12);
-        let val = imm+core.regs[rs1];
-
-        core.regs[rd] = core.regs[32]+4;
-        core.regs[32] = if val%2 == 0 {val} else {val-1};
-    }
-    else if opcode == Opcode::Branch as u32 {
-        let BType { imm, rs2, rs1, funct3 } = get_b_type(ins);
-
-        let imm = sign_extend(imm,13);
-        let target_addr = (((core.regs[32] + imm) as usize)%MEMSIZE) as i32;
-
-        if funct3 == Funct3::BEQ as u32 {
-            if core.regs[rs1] == core.regs[rs2] {
-                core.regs[32] = target_addr;
+            core.regs[rd] = core.regs[32]+4;
+            core.regs[32] = if val%2 == 0 {val} else {val-1};
+        },
+        opcodes::BRANCH => {
+            let BType { imm, rs2, rs1, funct3 } = get_b_type(ins);
+            let imm = sign_extend(imm,13);
+            let target_addr = (((core.regs[32] + imm) as usize)%MEMSIZE) as i32;
+            match funct3 {
+                funct3::BEQ => {
+                    if core.regs[rs1] == core.regs[rs2] {
+                        core.regs[32] = target_addr;
+                    }
+                },
+                funct3::BNE => {
+                    if core.regs[rs1] != core.regs[rs2] {
+                        core.regs[32] = target_addr;
+                    }
+                },
+                funct3::BLT => {
+                    if core.regs[rs1] < core.regs[rs2] {
+                        core.regs[32] = target_addr;
+                    }
+                },
+                funct3::BLTU => {
+                    if (core.regs[rs1] as u32) < (core.regs[rs2] as u32) {
+                        core.regs[32] = target_addr;
+                    }
+                },
+                funct3::BGE => {
+                    if core.regs[rs1] > core.regs[rs2] {
+                        core.regs[32] = target_addr;
+                    }
+                },
+                funct3::BGEU => {
+                    if (core.regs[rs1] as u32) > (core.regs[rs2] as u32) {
+                        core.regs[32] = target_addr;
+                    }
+                },
+                _ => {
+                    println!("Unknown Funct3 in Opcode Branch: {}", funct3);
+                }
             }
-        }
-        else if funct3 == Funct3::BNE as u32 {
-            if core.regs[rs1] != core.regs[rs2] {
-                core.regs[32] = target_addr;
+        },
+        opcodes::LOAD => {
+            let IType { imm, rs1, rd, funct3 } = get_i_type(ins);
+            let target_addr = ((sign_extend(imm,12)+core.regs[rs1]) as usize)%MEMSIZE;
+            match funct3 {
+                funct3::LB => {
+                    core.regs[rd] = (core.memory[target_addr] as i8) as i32;
+                },
+                funct3::LBU => {
+                    core.regs[rd] = core.memory[target_addr] as i32;
+                },
+                funct3::LH => {
+                    let b1 = core.memory[target_addr] as u16;
+                    let b2 = core.memory[target_addr+1] as u16;
+                    core.regs[rd] = (((b2<<8) | b1) as i16) as i32;
+                },
+                funct3::LHU => {
+                    let b1 = core.memory[target_addr] as u16;
+                    let b2 = core.memory[target_addr+1] as u16;
+                    core.regs[rd] = ((b2<<8) | b1) as i32;
+                },
+                funct3::LW => {
+                    let b1 = core.memory[target_addr] as u32;
+                    let b2 = core.memory[target_addr+1] as u32;
+                    let b3 = core.memory[target_addr+2] as u32;
+                    let b4 = core.memory[target_addr+3] as u32;
+                    core.regs[rd] = ((b4<<24) | (b3<<16) | (b2<<8) | b1) as i32;
+                },
+                _ => {
+                    println!("Unknown funct3 in Opcode Load: {}", funct3);
+                }
             }
-        }
-        else if funct3 == Funct3::BLT as u32 {
-            if core.regs[rs1] < core.regs[rs2] {
-                core.regs[32] = target_addr;
+        },
+        opcodes::STORE => {
+            let SType { imm, rs2, rs1, funct3 } = get_s_type(ins);
+            let target_addr = ((sign_extend(imm,12)+core.regs[rs1]) as usize)%MEMSIZE;
+            match funct3 {
+                funct3::SB => {
+                    core.memory[target_addr] = core.regs[rs2] as u8;
+                },
+                funct3::SH => {
+                    core.memory[target_addr] = core.regs[rs2] as u8;
+                    core.memory[target_addr+1] = (core.regs[rs2]>>8) as u8;
+                },
+                funct3::SW => {
+                    core.memory[target_addr] = core.regs[rs2] as u8;
+                    core.memory[target_addr+1] = (core.regs[rs2]>>8) as u8;
+                    core.memory[target_addr+2] = (core.regs[rs2]>>16) as u8;
+                    core.memory[target_addr+3] = (core.regs[rs2]>>24) as u8;
+                },
+                _ => {
+                    println!("Unknown funct3 in Opcode Store: {}", funct3);
+                }
             }
-        }
-        else if funct3 == Funct3::BLTU as u32 {
-            if (core.regs[rs1] as u32) < (core.regs[rs2] as u32) {
-                core.regs[32] = target_addr;
+        },
+        opcodes::MISCMEM => {
+            let funct3 = take_range(14,12,ins);
+            match funct3 {
+                funct3::FENCE => {
+                    /*
+                    * We do single core no cache so nothing to see here.
+                    */
+                },
+                _ => {
+                    println!("Unknown funct3 in Opcode MiscMem: {} ", funct3);
+                }
             }
+        },
+        _ => {
+            println!("Unknown opcode: {}", opcode);
         }
-        else if funct3 == Funct3::BGE as u32 {
-            if core.regs[rs1] > core.regs[rs2] {
-                core.regs[32] = target_addr;
-            }
-        }
-        else if funct3 == Funct3::BGEU as u32 {
-            if (core.regs[rs1] as u32) > (core.regs[rs2] as u32) {
-                core.regs[32] = target_addr;
-            }
-        }
-        else {
-            println!("Unknown Funct3 in Opcode Branch: {}", funct3);
-        }
-    }
-    else if opcode == Opcode::Load as u32 {
-        let IType { imm, rs1, rd, funct3: width } = get_i_type(ins);
-
-        let target_addr = ((sign_extend(imm,12)+core.regs[rs1]) as usize)%MEMSIZE;
-        if width == Funct3::LB as u32 {
-            core.regs[rd] = (core.memory[target_addr] as i8) as i32;
-        }
-        if width == Funct3::LBU as u32 {
-            core.regs[rd] = core.memory[target_addr] as i32;
-        }
-        else if width == Funct3::LH as u32 {
-            let b1 = core.memory[target_addr] as u16;
-            let b2 = core.memory[target_addr+1] as u16;
-            core.regs[rd] = (((b2<<8) | b1) as i16) as i32;
-        }
-        else if width == Funct3::LHU as u32 {
-            let b1 = core.memory[target_addr] as u16;
-            let b2 = core.memory[target_addr+1] as u16;
-            core.regs[rd] = ((b2<<8) | b1) as i32;
-        }
-        else if width == Funct3::LW as u32 {
-            let b1 = core.memory[target_addr] as u32;
-            let b2 = core.memory[target_addr+1] as u32;
-            let b3 = core.memory[target_addr+2] as u32;
-            let b4 = core.memory[target_addr+3] as u32;
-            core.regs[rd] = ((b4<<24) | (b3<<16) | (b2<<8) | b1) as i32;
-        }
-        else {
-            println!("Unknown width in Opcode Load: {}", width);
-        }
-    }
-    else if opcode == Opcode::Store as u32 {
-        let SType { imm, rs2, rs1, funct3: width } = get_s_type(ins);
-        let target_addr = ((sign_extend(imm,12)+core.regs[rs1]) as usize)%MEMSIZE;
-        if width == Funct3::SB as u32 {
-            core.memory[target_addr] = core.regs[rs2] as u8;
-        }
-        else if width == Funct3::SH as u32 {
-            core.memory[target_addr] = core.regs[rs2] as u8;
-            core.memory[target_addr+1] = (core.regs[rs2]>>8) as u8;
-        }
-        else if width == Funct3::SW as u32 {
-            core.memory[target_addr] = core.regs[rs2] as u8;
-            core.memory[target_addr+1] = (core.regs[rs2]>>8) as u8;
-            core.memory[target_addr+2] = (core.regs[rs2]>>16) as u8;
-            core.memory[target_addr+3] = (core.regs[rs2]>>24) as u8;
-        }
-        else {
-            println!("Unknown width in Opcode Store: {}", width);
-        }
-    }
-    else if opcode == Opcode::MiscMem as u32 {
-        let funct3 = take_range(14,12,ins);
-        if funct3 == Funct3::FENCE as u32 {
-            /*
-             * We do single core no cache so nothing to see here.
-             */
-        }
-        else {
-            println!("Unknown funct3 in Opcode MiscMem: {} ", funct3);
-        }
-
-    }
-    else {
-        println!("Unknown opcode: {}", opcode);
     }
     return core;
 }
