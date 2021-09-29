fa68308b34e5b4fff21d7c6ec6dfd0e4122af448
Jere Pehkonen <jeanpehk@gmail.com>
2021-09-29 20:23:57 +0300

    very hacky very bad but gets addresses

--- src/elf.rs
+++ src/elf.rs
@@ -41,21 +41,72 @@ pub fn parse_elf(core: &mut Core, elf: Vec<u8>) {
     println!("e_shstrndx: {:#x}", e_shstrndx);
 
     let mut index = e_shoff;
+    let mut sht_symtab = 0;
+    let mut sht_size = 0;
+    let mut sht_entsize = 0;
+    let mut sht_strtab = 0;
+    let mut sht_strsize = 0;
     for _ in 0..e_shnum {
-        println!("----------------------------------");
+      //  println!("----------------------------------");
         let sh_name = take4(&elf, index); // read(&elf, index);
         let sh_type = take4(&elf, index+0x04);
         let sh_addr = take4(&elf, index+0x0c);
+        let sh_offset = take4(&elf, index+0x10) as usize;
+        let sh_size = take4(&elf, index+0x14) as usize;
+        let sh_info = take4(&elf, index+0x1c);
+        let sh_entsize = take4(&elf, index+0x24) as usize;
+
+        /*
         println!("- sh_name: {:#x}", sh_name);
         println!("- sh_type: {:#x}", sh_type);
         println!("- sh_addr: {:#x}", sh_addr);
+        println!("- sh_offset: {:#x}", sh_offset);
+        println!("- sh_size: {:#x}", sh_size);
+        println!("- sh_info: {:#x}", sh_info);
+        println!("- sh_entsize: {:#x}", sh_entsize);
+        */
 
-        if sh_type == 0x2 { // symtab (todo)
+        if sh_type == 0x2 { // symtab
+            sht_symtab = sh_offset;
+            sht_size = sh_size;
+            sht_entsize = sh_entsize;
+        }
+        else if sh_type == 0x3 { // string table
+            if sht_strtab == 0 { // HACK HACK HACK FIX FIX FIX
+                sht_strtab = sh_offset;
+                sht_strsize = sh_size;
+            }
         }
 
         index += e_shentsize;
     }
-    println!("----------------------------------");
+
+    let mut ix = sht_symtab;
+    let mut fail_addr = 0;
+    let mut pass_addr = 0;
+    while ix < ix+sht_size {
+        let st_name = take4(&elf, ix) as usize;
+        let st_value = take4(&elf, ix+0x04);
+        let st_size = take4(&elf, ix+0x08);
+        let st_info = elf[ix+0x0c];
+        let st_other = elf[ix+0xd];
+        let st_shndx = take2(&elf, ix+0xe);
+        if st_name != 0 {
+            let ch = elf[sht_strtab+st_name];
+            let ch2 = elf[sht_strtab+st_name+1];
+
+            if ch as char == 'f' && ch2 as char == 'a' {
+                fail_addr = st_value;
+                if pass_addr != 0 { break };
+            }
+            else if ch as char == 'p' && ch2 as char == 'a' {
+                pass_addr = st_value;
+                if fail_addr != 0 { break };
+            }
+        }
+
+        ix += sht_entsize;
+    }
 }
 
 fn take4(elf: &Vec<u8>, i: usize) -> i32 {
